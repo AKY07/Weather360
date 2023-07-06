b71bac1015fcfd9f2fd668578f4f3de07bfaffde
AKY07 <imaalokd7@gmail.com>
2023-07-06 09:01:48 +0530

    Weather Part Done

---
+++ weather360/src/weatherService.js
@@ -0,0 +1,33 @@
+const API_KEY = "8f1251b6c062fe2f8bcbb77302f5f50b";
+const makeIconURL=(iconId)=>`https://openweathermap.org/img/wn/${iconId}@2x.png`
+
+
+
+const getFormattedWeatherData = async (city, units = "metric") => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+  const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+  const {
+    weather,
+    main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+    wind: { speed },
+    sys: { country },
+    name,
+  } = data;
+  const { description, icon } = weather[0];
+  return {
+    description,
+    iconURL:makeIconURL(icon),
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    pressure,
+    humidity,
+    speed,
+    country,
+    name,
+  };
+};
+export { getFormattedWeatherData };
